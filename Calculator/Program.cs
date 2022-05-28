dc841f92ff7ea80f25c35a9cdbe03de64b2bfdba
Марина <@WIN-B8VOSBH0DSO>
2022-05-28 16:55:47 +0300

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        //($"{number}*N={number * N}");
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number for show multiplication table:");
+            var userInput = Console.ReadLine();
+
+            var isNumber = int.TryParse(userInput, out int number);
+
+
+            Console.WriteLine("Enter number of multiplication order:");
+            var isInt = Console.ReadLine();
+            
+            var isN = int.TryParse(isInt, out int N);
+
+            if (isNumber & isN)
+            {                            
+                
+                PrintMultiplicationTable(N, number);
+                Console.WriteLine("Рrogram completed");               
+            }
+            else
+            {
+                Console.WriteLine("You enter not numbers");
+            }
+        }
+
+       static void PrintMultiplicationTable(int n, int a)
+        {
+            for (var i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"{a}*{i}={a * i}");
+            }
+        }  
+            
+    }
+}
---
+++ Homework №1 Mazurek Marina/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Homework__1_Mazurek_Marina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+
+            do
+            {
+                Console.WriteLine("Enter a number day of week: 1..7");
+            
+                if (int.TryParse(Console.ReadLine(), out int day))
+                {
+                    Console.WriteLine(GetDayOfWeek(day));
+                }
+                else
+                {
+                    Console.WriteLine("You enter not number");
+                }
+
+                Console.WriteLine("Do you want enter a number day of week again: Y/N");
+                answer = Console.ReadLine();
+                
+            } while (answer == "y" || answer == "Y");
+
+            Console.WriteLine("Programm is finished");
+        }
+
+        static string GetDayOfWeek(int a)
+        {
+            return a switch
+            {
+            1 => "Monday", 
+            2 => "Tuesday", 
+            3 => "Wednesday", 
+            4 => "Thursday",  
+            5 => "Friday",
+            6 => "Saturday",
+            7 => "Sunday",
+            _ => "You enter uncorrect number"
+            };
+        }
+}
+}
