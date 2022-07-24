4ce855555fee01e694f85721be5c7a8cf1d277e9
Marina Mazurek <m-arisha@list.ru>
2022-07-24 22:35:04 +0300

    add homework 5

--- HOMEWORK 5 Telephones/Terminal.cs
+++ HOMEWORK 5 Telephones/Terminal.cs
@@ -5,6 +5,7 @@
         public Port Port { get; set; }                
         public int PhoneNumber { get; set; }
         public bool IsOnCall { get; set; }
+
         public Terminal(Port port, int phoneNumber)
         {
             Port = port;
